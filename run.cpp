38e5ca76a49e49e5b280b7eb36b1b66be427129d
trgchinhh <chinhcuber@gmail.com>
2026-07-24 11:17:04 +0700

    Hoàn thành thu gọn vào folder chia module và src giúp tối giản hóa thư mục

--- run.cpp
+++ run.cpp
@@ -1,8 +1,10 @@
-#include <cstdlib>
+#include <iostream>
 using namespace std;
 
+string lenhbiendich = "g++ src\\chuongtrinh.cpp src\\caidat.cpp src\\blockchain.cpp src\\lib\\qrcodegen.cpp -o chuongtrinh.exe" ;
+
 int main(){
-    system("g++ chuongtrinh.cpp caidat.cpp blockchain.cpp lib\\qrcodegen.cpp -o chuongtrinh.exe");
+    system(lenhbiendich.c_str());
     system("chuongtrinh.exe");
     return 0;
 }
---
+++ src/lib/blockchain.h
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include "json.hpp"
+
+using json = nlohmann::ordered_json;
+using namespace std;
+
+const int do_kho_mac_dinh = 5;
+const string file_block = "src\\data\\block.json";
+
+struct GiaoDich {
+    string nguoi_gui;
+    string nguoi_nhan;
+    string stk_gui;
+    string stk_nhan;
+    int so_tien;
+    string thoigian;
+
+    GiaoDich(string nguoi_gui, string nguoi_nhan, string stk_gui, string stk_nhan, double so_tien, string thoigian);
+};
+
+struct Block {
+    int chi_so;
+    string ma_truoc;
+    GiaoDich giaodich;
+    string thoigian;
+    int nonce;
+    string ma_hash;
+
+    Block(int chi_so, string ma_truoc, GiaoDich giaodich);
+    string tinhHash() const;
+};
+
+class Blockchain {
+private:
+    vector<Block> chuoi;
+    int do_kho;
+
+    Block taoBlockGoc();
+    Block layBlockCuoi() const;
+    void daoBlock(Block &block);
+
+public:
+    Blockchain(int do_kho = do_kho_mac_dinh);
+    void docFile(const string &tenfile);
+    void themGiaoDich(const GiaoDich &gd);
+    bool kiemTraHopLe() const;
+    void luuFile(const string &tenfile) const;
+    void inChuoi() const;
+};
